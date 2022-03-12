f74e8cb2146e96b15daa7cfaf8291712448a9ef3
Pedro Teixeira <peteixeira@hubspot.com>
2022-03-12 16:59:42 +0000

    improved this web app a lot

--- project/js/main.js
+++ project/js/main.js
@@ -3,7 +3,7 @@ function myFunction() {
   // use jQuery ($ is shorthand) to find the div on the page and then change the html
   // jQuery can do a lot of crazy stuff so make sure to google around to find out more
 
-  $("#demo").append("NEWWW PARAGRAPH #javascript #fire");
+  $("#demo").append("International Women's Day Hoppers Event");
 
   // 'img-circle' is a bootstrap thing! Check out more here: http://getbootstrap.com/css/
   $("#doge-image").append(`<img src="https://media3.giphy.com/media/a0h7sAqON67nO/giphy.gif?cid=ecf05e47a4ltnqvpzes3gg8dvvc8ktd7z1t2mc2ve3d1n1fi&rid=giphy.gif&ct=g" />`);
